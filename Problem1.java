2aece20f111075768abf8f8623ba9d3588712d53
wjun-kim <132063890+wjun-kim@users.noreply.github.com>
2023-05-26 22:06:34 +0900

    Update Problem1.java

--- Problem1.java
+++ Problem1.java
@@ -1,53 +1,26 @@
 import java.util.*;
 
 public class week13_1 {
-    public static void main(String[] args) {
+     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
         // 필요한 자료형을 넣어주세요
-        ArrayList<Number> list = new ArrayList<Number>();
+        ArrayList< Number > list = new ArrayList<>();
         System.out.print("Enter five integers and five doubles: ");
-
-        for (int i = 0; i < 5; i++){
-            int num = input.nextInt();
-            list.add(num);
+        for(int i = 0; i < 5; i++){
+          list.add(input.nextInt());
         }
-        for (int i = 0; i < 5; i++){
-            double num = input.nextDouble();
-            list.add(num);
-        }
+        for(int i = 0; i < 5; i++){
+            list.add(input.nextDouble());
+          }
+      
 
         sort(list);
 
-        for (int i = 0; i < 10; i++){
+        for(int i =0; i < 10; i++){
             System.out.print(list.get(i));
-            System.out.print(" ");
-    }
-
-   import java.util.*;
-
-public class week13_12 {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-
-        // 필요한 자료형을 넣어주세요
-        ArrayList<Number> list = new ArrayList<Number>();
-        System.out.print("Enter five integers and five doubles: ");
-
-        for (int i = 0; i < 5; i++){
-            int num = input.nextInt();
-            list.add(num);
+            System.out.print(' ');
         }
-        for (int i = 0; i < 5; i++){
-            double num = input.nextDouble();
-            list.add(num);
-        }
-
-        sort(list);
-
-        for (int i = 0; i < 10; i++){
-            System.out.print(list.get(i));
-            System.out.print(" ");
     }
 
     public static void sort(ArrayList<Number> list) {
@@ -58,7 +31,5 @@ public class week13_12 {
            }
        });
     }
-}
-
 }
 
